6bd7232563e5719755afd9b5163629ae09fdb429
Vinicius Reis <6201339+viniciusreis@users.noreply.github.com>
2024-05-01 20:53:02 -0300

    Change lualine colors.

--- .config/nvim/lua/vcr/lazy/lualine.lua
+++ .config/nvim/lua/vcr/lazy/lualine.lua
@@ -3,25 +3,25 @@ return {
   config = function()
 
     local new_colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      black = "#000000",
+      normal = "#D5AA61",
+      insert = "#CB5D46",
+      visual = "#5AA297",
+      commandbg = "#E4844D",
+      commandfg = "#000000",
     }
 
     local lualine = require("lualine")
     local nightfly = require('lualine.themes.nightfly')
 
-    nightfly.normal.a.bg = new_colors.blue
-    nightfly.insert.a.bg = new_colors.green
-    nightfly.visual.a.bg = new_colors.violet
+    nightfly.normal.a.bg = new_colors.normal
+    nightfly.insert.a.bg = new_colors.insert
+    nightfly.visual.a.bg = new_colors.visual
 
     nightfly.command = {
       a = {
         gui = "bold",
-        bg = new_colors.yellow,
-        fg = new_colors.black,
+        bg = new_colors.commandbg,
+          fg = new_colors.commandfg,
       }
     }
 
